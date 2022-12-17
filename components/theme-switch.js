6329394fc7a5216ee0ded0b550be62024b69d925
Roldão Rego Jr <roldaogjr@gmail.com>
2022-12-17 14:43:59 -0300

    Dark mode detection

--- components/theme-switch.js
+++ components/theme-switch.js
@@ -2,7 +2,7 @@ import { useTheme as useNextTheme } from 'next-themes'
 import { Switch, useTheme } from '@nextui-org/react'
 import { SunIcon } from '../icons/SunIcon'
 import { MoonIcon } from '../icons/MoonIcon'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 
 export const ThemeSwitch = () => {
@@ -11,6 +11,19 @@ export const ThemeSwitch = () => {
   const changeTheme = useCallback(e => {
     setTheme(e.target.checked ? 'dark' : 'light')
   }, [setTheme])
+  const darkModeListener = useCallback(e => {
+    setTheme(e.matches ? "dark": "light")
+  }, [setTheme])
+  useEffect(() => {
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener(
+      'change', darkModeListener
+    )
+    return () => {
+      window.matchMedia('(prefers-color-scheme: dark)').removeEventListener(
+        'change', darkModeListener
+      )
+    }
+  }, [])
   return (
     <Switch
       color="default"
